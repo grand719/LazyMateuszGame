7b51e6bca34d22040aff5b37501acb14a8774e62
mateusz.wojda <mateusz.wojda@vidaa.com>
2025-04-21 23:20:13 +0200

    Level design, byczq

---
+++ src/game/mapAssets/tvs/FrontWideTVLeft.ts
@@ -0,0 +1,18 @@
+import Actor from "../../../engine/framework/Actor";
+import { Vector2d } from "../../../engine/framework/types";
+import World from "../../../engine/framework/World";
+
+export default class FrontWideTVLeft extends Actor {
+  constructor(owningWorld: World, startingPosition: Vector2d) {
+    super(
+      "/tvs/FrontWideTVLeft.png",
+      owningWorld,
+      startingPosition,
+      true,
+      0,
+      { x: 0, y: 0 },
+      { x: 300, y: 300 },
+      { x: 0, y: 0 }
+    );
+  }
+}
